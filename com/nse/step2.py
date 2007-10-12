7257bfb1a0231a5ef31b5f82141f4e059c2c44cc
tgolden <tgolden@e265957e-b7df-11dd-a503-ed2d4bea8bb5>
2007-10-12 13:37:07 +0000

    Added Python project for exploring COM, initially Shell Namespace extensions
    added      .
    added      nse
    added      nse/shell_view.py
    added      nse/step1.py
    added      nse/step2.py

---
+++ com/nse/step2.py
@@ -0,0 +1,130 @@
+import sys, os
+import pythoncom
+import winerror
+from win32com.shell import shell, shellcon
+from win32com.server.util import wrap, NewEnum
+from win32com.server.exception import COMException
+
+def filepath_to_shell (filepath):
+  """Convert a filepath into the shell equivalent: an IShellFolder and
+  the corresponding absolute PIDL
+  """
+  desktop = shell.SHGetDesktopFolder ()
+  _, pidl, _ = desktop.ParseDisplayName (0, None, filepath)
+  
+  shell_folder = desktop
+  while len (pidl) > 1:
+    this = pidl.pop (0)
+    shell_folder = shell_folder.BindToObject ([this], None, shell.IID_IShellFolder)
+    
+  return shell_folder, pidl
+
+IPersist_Methods = ["GetClassID"]
+IPersistFolder_Methods = IPersist_Methods + ["Initialize"]
+IShellFolder_Methods = [
+  "CreateViewObject",
+  "EnumObjects",
+  "ParseDisplayName",
+  "BindToObject",
+  "BindToStorage",
+  "CompareIDs", 
+  "GetUIObjectOf",
+  "GetDisplayNameOf",
+  "GetAttributesOf",
+  "SetNameOf",
+]
+
+class ShellFolderRoot:
+  _reg_progid_ = "TJG.Bookmarks"
+  _reg_desc_ = "TJG Bookmarks"
+  _reg_clsid_ = "{398ADD84-73CB-4d63-BA91-F42D9D986A3E}"
+
+  _com_interfaces_ = [
+    pythoncom.IID_IPersist,
+    shell.IID_IPersistFolder,
+    shell.IID_IShellFolder,
+  ]
+  _public_methods_ = IPersistFolder_Methods + IShellFolder_Methods
+
+  bookmarks_filepath = "c:/temp/bookmarks.txt"
+  
+  def GetClassID(self):
+    return self._reg_clsid_
+  
+  def Initialize(self, pidl):
+    self.pidl = pidl
+    if os.path.exists (self.bookmarks_filepath):
+      self.bookmarks = open (self.bookmarks_filepath).read ().splitlines ()
+    else:
+      self.bookmarks = []
+  
+  def CreateViewObject(self, hwnd, iid):
+    raise COMException (hresult=winerror.E_NOTIMPL)
+  
+  def EnumObjects(self, hwndOwner, flags):
+    return NewEnum (self.bookmarks, iid=shell.IID_IEnumIDList)
+  
+  def ParseDisplayName (self, hwnd, reserved, displayName, attr):
+    raise COMException (hresult=winerror.E_NOTIMPL)
+  
+  def BindToStorage(self, pidl, bc, iid):
+    raise COMException (hresult=winerror.E_NOTIMPL)
+  
+  def BindToObject(self, pidl, bc, iid):
+    raise COMException (hresult=winerror.E_NOTIMPL)
+
+  def CompareIDs(self, param, id1, id2):
+    return cmp (id1, id2)
+  
+  def GetUIObjectOf(self, hwndOwner, pidls, iid, inout):
+    raise COMException (hresult=winerror.E_NOTIMPL)
+
+  def GetDisplayNameOf(self, pidl, flags):
+    raise COMException (hresult=winerror.E_NOTIMPL)
+  
+  def GetAttributesOf(self, pidls, attrFlags):
+    raise COMException (hresult=winerror.E_NOTIMPL)
+
+def DllRegisterServer():
+  import _winreg
+  key = _winreg.CreateKey (
+    _winreg.HKEY_LOCAL_MACHINE,
+    "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\" \
+      "Explorer\\Desktop\\Namespace\\" + \
+      ShellFolderRoot._reg_clsid_
+  )
+  _winreg.SetValueEx (key, None, 0, _winreg.REG_SZ, ShellFolderRoot._reg_desc_)
+  # And special shell keys under our CLSID
+  key = _winreg.CreateKey (
+    _winreg.HKEY_CLASSES_ROOT,
+    "CLSID\\" + ShellFolderRoot._reg_clsid_ + "\\ShellFolder"
+  )
+  # 'Attributes' is an int stored as a binary! use struct
+  attr = shellcon.SFGAO_FOLDER | shellcon.SFGAO_HASSUBFOLDER | shellcon.SFGAO_BROWSABLE
+  import struct
+  s = struct.pack ("i", attr)
+  _winreg.SetValueEx (key, "Attributes", 0, _winreg.REG_BINARY, s)
+  print ShellFolderRoot._reg_desc_, "registration complete."
+
+def DllUnregisterServer():
+  import _winreg
+  try:
+    key = _winreg.DeleteKey (
+      _winreg.HKEY_LOCAL_MACHINE,
+      "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\" \
+        "Explorer\\Desktop\\Namespace\\" + \
+        ShellFolderRoot._reg_clsid_
+    )
+  except WindowsError, details:
+    import errno
+    if details.errno != errno.ENOENT:
+      raise
+  print ShellFolderRoot._reg_desc_, "unregistration complete."
+
+if __name__=='__main__':
+  from win32com.server import register
+  register.UseCommandLine (
+    ShellFolderRoot,
+    finalize_register = DllRegisterServer,
+    finalize_unregister = DllUnregisterServer
+  )
