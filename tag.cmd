49eb8272758092519da911e8f4d4f95fbf35fd0d
tgolden <tgolden@e265957e-b7df-11dd-a503-ed2d4bea8bb5>
2008-01-14 10:35:56 +0000

    Actually commit tagged release
    modified   tag.cmd

--- tag.cmd
+++ tag.cmd
@@ -3,7 +3,7 @@ if "%1" == "" goto error
 if "%2" == "" goto error
 
 svn copy %1\trunk %1\tags\%2
-echo svn commit %1\tags\%2 -m "Tagged for release %2"
+svn commit %1\tags\%2 -m "Tagged for release %2"
 chdir %1\tags
 zip -r %1-%2.zip %2
 pscp %1-%2.zip tgolden@scandium:web/timgolden.me.uk/python/downloads
