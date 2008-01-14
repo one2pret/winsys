a14591141d5d43b487e4e006bc6ce34f52bb21a6
tgolden <tgolden@e265957e-b7df-11dd-a503-ed2d4bea8bb5>
2008-01-14 11:05:24 +0000

    Use externals of downloads to copy releases
    modified   .
    modified   tag.cmd

--- tag.cmd
+++ tag.cmd
@@ -6,9 +6,10 @@ svn copy %1\trunk %1\tags\%2
 svn commit %1\tags\%2 -m "Tagged %1 for release %2"
 chdir %1\tags
 zip -r %1-%2.zip %2
-pscp %1-%2.zip tgolden@scandium:web/timgolden.me.uk/python/downloads
-del %1-%2.zip
 chdir ..\..
+move %1\tags\%1-%2.zip downloads
+svn add downloads\%1-%2.zip
+svn commit downloads\%1-%2.zip -m "Zipped release %2 of %1"
 
 goto finish
 
