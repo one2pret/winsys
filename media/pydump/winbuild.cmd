6acf5e47d94c55833430284373d6c9e3c109d22b
tgolden <tgolden@e265957e-b7df-11dd-a503-ed2d4bea8bb5>
2008-01-14 10:04:52 +0000

    Remove media from python-projects: ported over to Cas
    deleted    media
    deleted    media/dump.c
    deleted    media/media.py
    deleted    media/media_player.py
    deleted    media/media_scanner.ini
    deleted    media/media_scanner.py
    deleted    media/pydump
    deleted    media/pydump/build.cmd
    deleted    media/pydump/ffmpeg.pyx
    deleted    media/pydump/pydump.pyx
    deleted    media/pydump/s.avi
    deleted    media/pydump/setup-t.py
    deleted    media/pydump/setup.py
    deleted    media/pydump/test.py
    deleted    media/pydump/udump.c
    deleted    media/pydump/winbuild.cmd
    deleted    media/scan-media.py
    deleted    media/winbuild.cmd

--- media/pydump/winbuild.cmd
+++
@@ -1,1 +0,0 @@
-gcc -o udump -s -Ic:\msys\1.0\local\include udump.c -Lc:\msys\1.0\local\lib -lavformat -lavcodec -lavutil -lwsock32
--- media/winbuild.cmd
+++
@@ -1,3 +0,0 @@
-gcc -o dump -s -Ic:\msys\1.0\local\include dump.c -Lc:\msys\1.0\local\lib -lavformat -lavcodec -lavutil -lwsock32 && dump.exe t3.m2v
-pause
-
