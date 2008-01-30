d87da8920a1a44a3046271db1c92d48d305f0436
tgolden <tgolden@e265957e-b7df-11dd-a503-ed2d4bea8bb5>
2008-01-30 17:28:52 +0000

    Updates to pre-process step for pywin32-docs
    modified   .
    modified   preprocess.py

--- pywin32-docs/preprocess.py
+++ pywin32-docs/preprocess.py
@@ -1,24 +1,33 @@
 import os, sys
 import glob
 import re
+import htmlentitydefs
 
-def munge_html_files (dirname, filenames):
-  for filename in filenames:
-    filepath = os.path.join (dirname, filename)
-    print filepath
-    text = open (filepath).read ()
-    text = re.sub ("(\n+)", "\n", text)
-    text = re.sub ("(<META[^>]+>)", "\g<1>\n</head>\n<body>\n", text)
-    open (filepath, "w").write (text)
+import html2rst
+
+def munged_text (text):
+  text = re.sub (r"(\n+)", r"\n", text)
+  text = re.sub (r"(<META[^>]+>)", r"\g<1>\n</head>\n<body>\n", text)
+  for entitydef in htmlentitydefs.entitydefs.keys ():
+    text = re.sub (r"(&%s[^;])" % entitydef, "&%s;" % entitydef, text)
+  text = re.sub (r"(&#\d+)[^;]", "\g<1>;", text)
+  return html2rst.html2text (text)
 
 def main (args=None):
   chm_filepath = os.path.join (sys.prefix, "lib", "site-packages", "PyWin32.chm")
-  tempdir = "temp"
-  if not os.path.exists (tempdir):
-    os.mkdir (tempdir)
-  os.system ("hh.exe -decompile %s %s" % (tempdir, chm_filepath))
-  for dirname, dirnames, filenames in os.walk (tempdir):
-    munge_html_files (dirname, filenames)
+  html_tempdir = "htmltemp"
+  rst_tempdir = "rsttemp"
+  if not os.path.exists (html_tempdir): os.mkdir (html_tempdir)
+  if not os.path.exists (rst_tempdir): os.mkdir (rst_tempdir)
+  
+  os.system ("hh.exe -decompile %s %s" % (html_tempdir, chm_filepath))
+  for dirname, dirnames, filenames in os.walk (html_tempdir):
+    for filename in filenames:
+      print filename
+      base, ext = os.path.splitext (filename)
+      open (os.path.join (rst_tempdir, "%s.rst" % base), "w").write (
+        munged_text (open (os.path.join (dirname, filename)).read ())
+      )
   
 if __name__ == '__main__':
   main (sys.argv[1:])
