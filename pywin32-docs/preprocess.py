bca9de7637167ceff2b35712554e93a75011720e
tgolden <tgolden@e265957e-b7df-11dd-a503-ed2d4bea8bb5>
2008-02-01 15:57:14 +0000

    Rework preprocess to use BeautifulSoup for parsing
    modified   preprocess.py

--- pywin32-docs/preprocess.py
+++ pywin32-docs/preprocess.py
@@ -4,47 +4,41 @@ import re
 import htmlentitydefs
 
 import html2rst
+from BeautifulSoup import MinimalSoup
 
 def munged_text (text):
-  text = re.sub (r"(\n+)", r"\n", text)
-  text = re.sub (r"(<META[^>]+>)", r"\g<1>\n</head>\n<body>\n", text)
+  #
+  # Fix up entity & character defs so they end with semicolons
+  #
   for entitydef in htmlentitydefs.entitydefs.keys ():
-    text = re.sub (r"(&%s[^;])" % entitydef, "&%s;" % entitydef, text)
-  text = re.sub (r"(&#\d+)[^;]", "\g<1>;", text)
-  
-  #
-  # Fixup unclosed <LI>
-  #
-  lines = []
-  for line in text.lower ().splitlines ():
-    if line.startswith ("<li>") and not line.endswith ("</li>"):
-      lines.append (line + "</li>")
-    else:
-      lines.append (line)
-  text = "\n".join (lines)
-  
-  return html2rst.html2text (text)
+    text = re.sub (r"(&%s)(?!;)" % entitydef, "\g<1>;", text)
+  text = re.sub (r"(&#\d+)(?!;)", "\g<1>;", text)  
+  return html2rst.html2text (unicode (MinimalSoup (text)))
 
 def main (args=None):
   chm_filepath = os.path.join (sys.prefix, "lib", "site-packages", "PyWin32.chm")
   html_tempdir = "htmltemp"
   rst_tempdir = "rsttemp"
-  if not os.path.exists (html_tempdir): os.mkdir (html_tempdir)
-  if not os.path.exists (rst_tempdir): os.mkdir (rst_tempdir)
   
-  os.system ("hh.exe -decompile %s %s" % (html_tempdir, chm_filepath))
-  for dirname, dirnames, filenames in os.walk (html_tempdir):
+  if not os.path.exists (html_tempdir): 
+    os.mkdir (html_tempdir)
+    os.system ("hh.exe -decompile %s %s" % (html_tempdir, chm_filepath))
+  
+  if not os.path.exists (rst_tempdir): 
+    os.mkdir (rst_tempdir)
+  for html_dirname, dirnames, filenames in os.walk (html_tempdir):
+    rst_dirname = os.path.join (rst_tempdir, html_dirname[1+len (html_tempdir):])
+    print html_dirname, "=>", rst_dirname
+    if not os.path.exists (rst_dirname):
+      os.mkdir (rst_dirname)
     for filename in filenames:
       if not filename.endswith (".html"): continue
-      print filename
+      filepath = os.path.join (html_dirname, filename)
+      print "  ", filepath
       base, ext = os.path.splitext (filename)
-      try:
-        open (os.path.join (rst_tempdir, "%s.rst" % base), "w").write (
-          munged_text (open (os.path.join (dirname, filename)).read ()).encode ("utf8")
-        )
-      except:
-        print repr (munged_text (open (os.path.join (dirname, filename)).read ()))
-        raise
+      open (os.path.join (rst_dirname, "%s.rst" % base), "w").write (
+        munged_text (open (filepath).read ()).encode ("utf8")
+      )
   
 if __name__ == '__main__':
   main (sys.argv[1:])
