10425b146f0d64f1d85edd02972d5200db98bb58
tgolden <tgolden@e265957e-b7df-11dd-a503-ed2d4bea8bb5>
2008-02-05 14:23:59 +0000

    Almost-working AccessCheck example as Python extension
    modified   extension.c

--- file-extension/extension.c
+++ file-extension/extension.c
@@ -3,82 +3,92 @@
 #include <windows.h>
 
 static PyObject*
-extension_attribs(PyObject *self, PyObject *args)
-{
-  LPCTSTR lpFileName;
-  DWORD result;
-  
-  if (!PyArg_ParseTuple (args, "s", &lpFileName)) {
-    return NULL;
-  }
-  
-  if ((result = GetFileAttributes (lpFileName)) == INVALID_FILE_ATTRIBUTES) {
-    PyErr_SetString (PyExc_RuntimeError, "No such file");
-    return NULL;
-  }
-  if (result & FILE_ATTRIBUTE_READONLY) {
-    return Py_BuildValue ("s", "READONLY");
-  }
-  else
-  {
-    return Py_BuildValue ("s", "NOT-READONLY");
-  }
-}
-
-static PyObject*
 windows_file_security (PyObject *self, PyObject *args)
 {
-	SECURITY_INFORMATION requested_information = DACL_SECURITY_INFORMATION;
-	PSECURITY_DESCRIPTOR security_descriptor = NULL;
-	DWORD dwSize = 0;
-	//~ HANDLE hToken;
-	//~ DWORD access_desired;
+    SECURITY_INFORMATION requested_information = DACL_SECURITY_INFORMATION |
+        OWNER_SECURITY_INFORMATION | GROUP_SECURITY_INFORMATION;
+    PSECURITY_DESCRIPTOR security_descriptor = NULL;
+    DWORD dwSize = 0;
+    char exception_string[256];
+    HANDLE hToken = INVALID_HANDLE_VALUE;
+    DWORD access_desired = 0;
 
-	//~ GENERIC_MAPPING mapping;
-	//~ mapping.GenericRead = 0; 
-	//~ mapping.GenericWrite = GENERIC_WRITE;
-	//~ mapping.GenericExecute = 0;
-	//~ mapping.GenericAll = 0;
-  //~ PRIVILEGE_SET privilege_set;
+    GENERIC_MAPPING mapping;
+    PRIVILEGE_SET privilege_set;
+    DWORD privilege_set_size = sizeof (privilege_set);
+    DWORD access_granted = 0;
+    BOOL is_access_granted = FALSE;
   
-  PyObject *ofilepath;
-  LPCWSTR *filepath;
+    PyObject *filepath;
 	
-  if (!PyArg_UnpackTuple (args, "windows_file_security", 1, 1, &filepath)) {
-    return NULL;
-  }
+    if (!PyArg_UnpackTuple (args, "windows_file_security", 1, 1, &filepath)) {
+        goto finish;
+    }
   
-  if (!filepath = PyString_AsString (ofilepath)) {
-    return NULL
-  }
-  
-  if (GetFileSecurityW (filepath, requested_information, security_descriptor, dwSize, &dwSize)) {
-    PyErr_SetString (PyExc_RuntimeError, "Problem getting security");
-		return NULL;
-  }
-	//~ security_descriptor = (PSECURITY_DESCRIPTOR)malloc(dwSize);
-	//~ if (security_descriptor == NULL)
-		//~ return -2;
-	//~ if (!GetFileSecurityW (PyUnicode_AS_UNICODE (filepath), requested_information, security_descriptor, dwSize, &dwSize))
-		//~ return -3;
-	//~ if (!ImpersonateSelf (SecurityImpersonation))
-		//~ return -4;
-	//~ if (!OpenThreadToken (GetCurrentThread (), TOKEN_ALL_ACCESS, TRUE, &hToken))
-		//~ return -5;
-	//~ MapGenericMask (&access_desired, &mapping);
+    if (!PyUnicode_Check (filepath) && !PyString_Check (filepath)) {
+        PyErr_SetString (PyExc_RuntimeError, "File must be string or unicode");
+        goto finish;
+    }
+    
+    if (PyUnicode_Check (filepath)) {
+        GetFileSecurityW (PyUnicode_AS_UNICODE (filepath),
+            requested_information, 0, 0, &dwSize);
+        security_descriptor = (PSECURITY_DESCRIPTOR)malloc(dwSize);
+        if (!GetFileSecurityW (PyUnicode_AS_UNICODE (filepath),
+            requested_information, security_descriptor, dwSize, &dwSize)) {
+            sprintf (exception_string, "FileSecurity: %d\n", GetLastError ());
+            PyErr_SetString (PyExc_RuntimeError, exception_string);
+            goto finish;
+        }
+    }
+    else {
+        PyErr_SetString (PyExc_RuntimeError, "Only working with unicode");
+        goto finish;
+    }
+    
+    if (!ImpersonateSelf (SecurityImpersonation)) {
+        PyErr_SetString (PyExc_RuntimeError, "Unable to impersonate self");
+        goto finish;
+    }
+    
+    if (!OpenThreadToken (GetCurrentThread (), TOKEN_ALL_ACCESS, 
+        TRUE, &hToken)) {
+        PyErr_SetString (PyExc_RuntimeError, "Unable to open thread token");
+        goto finish;
+    }
 
-	//~ RevertToSelf ();
-	//~ free (security_descriptor);
-	//~ return 1;
-  
-  Py_INCREF (filepath);
-  return filepath;
+    mapping.GenericRead = FILE_GENERIC_READ; 
+    mapping.GenericWrite = FILE_GENERIC_WRITE;
+    mapping.GenericExecute = FILE_GENERIC_EXECUTE;
+    mapping.GenericAll = FILE_ALL_ACCESS;
+	  MapGenericMask (&access_desired, &mapping);
+    
+    if (!AccessCheck (
+        security_descriptor, hToken, access_desired, &mapping, &privilege_set,
+        &privilege_set_size, &access_granted, &is_access_granted)) {
+        
+        sprintf (exception_string, "AccessCheck: %d\n", GetLastError ());
+        PyErr_SetString (PyExc_RuntimeError, exception_string);
+        goto finish;
+    }
+    
+finish:
+    RevertToSelf ();
+    if (security_descriptor) {
+      free (security_descriptor);
+    }
+    if (hToken != INVALID_HANDLE_VALUE) {
+        CloseHandle (hToken);
+    }
+
+    if (PyErr_Occurred()) {
+        return NULL;
+    } else {
+        return Py_BuildValue ("i", is_access_granted);
+    }
 }
 
-
-
 static PyMethodDef extension_methods[] = {
-  {"attribs", extension_attribs, METH_VARARGS, "Get attributes for a file"},
   {"windows_file_security", windows_file_security, METH_VARARGS, "Get windows file security"},
   {NULL, NULL}
 };
