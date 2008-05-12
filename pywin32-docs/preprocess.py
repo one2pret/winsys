c0919bf9f81bceeda604f7969dc3489fcde1ea09
tgolden <tgolden@e265957e-b7df-11dd-a503-ed2d4bea8bb5>
2008-05-12 16:03:48 +0000

    Small mods to preprocess for pywin32 docs
    modified   preprocess.py

--- pywin32-docs/preprocess.py
+++ pywin32-docs/preprocess.py
@@ -1,10 +1,16 @@
 import os, sys
 import glob
-import re
 import htmlentitydefs
+import re
+import traceback
 
 import html2rst
 from BeautifulSoup import MinimalSoup
+
+UNWANTED_MARKUP = ["html", "body", "head"]
+UNWANTED_RE = re.compile ("|".join ("<%s>|</%s>" % (markup, markup) for markup in UNWANTED_MARKUP), re.IGNORECASE)
+UNWANTED_TITLE_RE = re.compile (r"<title>.*</title>", re.IGNORECASE)
+UNWANTED_LEADING_HR = re.compile (r"^-*\n*")
 
 def munged_text (text):
   #
@@ -12,14 +18,24 @@ def munged_text (text):
   #
   for entitydef in htmlentitydefs.entitydefs.keys ():
     text = re.sub (r"(&%s)(?!;)" % entitydef, "\g<1>;", text)
-  text = re.sub (r"(&#\d+)(?!;)", "\g<1>;", text)  
-  soup = unicode (MinimalSoup (text))
-  print soup
-  rst = html2rst.html2text (soup)
-  if not rst.strip ():
-    raise RuntimeError, "No RST"
+  text = re.sub (r"(&#\d+)(?!;)", "\g<1>;", text)
+  text = re.sub (r"<title>[^<]*</title>", "", text, re.IGNORECASE)
+  text = UNWANTED_RE.sub ("", text)
+  text = UNWANTED_TITLE_RE.sub ("", text)
+  text = "\n".join (line + "</li>" if line.lower ().startswith ("<li>") and "</li>" not in line.lower () else line for line in text.splitlines ())
+  soup = unicode (text, "cp1252")
+  
+  try:
+    rst = html2rst.html2text (soup)
+  except:
+    print soup
+    raise
   else:
-    return rst
+    rst = UNWANTED_LEADING_HR.sub ("", rst)
+    if not rst.strip ():
+      raise RuntimeError, "No RST"
+    else:
+      return rst
 
 def main (args=None):
   chm_filepath = os.path.join (sys.prefix, "lib", "site-packages", "PyWin32.chm")
@@ -32,19 +48,25 @@ def main (args=None):
   
   if not os.path.exists (rst_tempdir): 
     os.mkdir (rst_tempdir)
+  
   for html_dirname, dirnames, filenames in os.walk (html_tempdir):
     rst_dirname = os.path.join (rst_tempdir, html_dirname[1+len (html_tempdir):])
     print html_dirname, "=>", rst_dirname
     if not os.path.exists (rst_dirname):
       os.mkdir (rst_dirname)
+    
     for filename in filenames:
       if not filename.endswith (".html"): continue
-      filepath = os.path.join (html_dirname, filename)
-      print "  ", filepath
+      html_filepath = os.path.join (html_dirname, filename)
+      print "  ", html_filepath
       base, ext = os.path.splitext (filename)
-      open (os.path.join (rst_dirname, "%s.rst" % base), "w").write (
-        munged_text (open (filepath).read ()).encode ("utf8")
-      )
+      rst_filepath = os.path.join (rst_dirname, "%s.rst" % base)
+      if os.path.exists (rst_filepath) and os.path.getmtime (rst_filepath) > os.path.getmtime (html_filepath):
+        continue
+      else:
+        open (rst_filepath, "w").write (
+          munged_text (open (html_filepath).read ()).encode ("utf8")
+        )
   
 if __name__ == '__main__':
   main (sys.argv[1:])
