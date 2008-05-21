4c1aafa106cbef390bddfa9ddfbe96d7cd62d877
tgolden <tgolden@e265957e-b7df-11dd-a503-ed2d4bea8bb5>
2008-05-21 16:27:32 +0000

    sphinx-docs now moved to Google Code
    deleted    sphinx-docs
    deleted    sphinx-docs/.build
    deleted    sphinx-docs/.static
    deleted    sphinx-docs/.static/default.css
    deleted    sphinx-docs/.templates
    deleted    sphinx-docs/acknowledgements.rst
    deleted    sphinx-docs/build.cmd
    deleted    sphinx-docs/building-extensions
    deleted    sphinx-docs/conf.py
    deleted    sphinx-docs/getting-started.rst
    deleted    sphinx-docs/index.rst
    deleted    sphinx-docs/programming-areas
    deleted    sphinx-docs/programming-areas/index.rst
    deleted    sphinx-docs/programming-areas/registry
    deleted    sphinx-docs/programming-areas/registry.rst
    deleted    sphinx-docs/programming-areas/registry/copy_the_registry.py
    deleted    sphinx-docs/programming-areas/registry/delete-the-registry.rst
    deleted    sphinx-docs/programming-areas/registry/delete_the_registry_shell.py
    deleted    sphinx-docs/programming-areas/registry/delete_the_registry_winreg.py
    deleted    sphinx-docs/programming-areas/registry/pysoft.reg
    deleted    sphinx-docs/programming-areas/registry/read_the_registry.py
    deleted    sphinx-docs/programming-areas/registry/restore_the_registry.py
    deleted    sphinx-docs/programming-areas/registry/save-and-restore-the-registry.rst
    deleted    sphinx-docs/programming-areas/registry/save_the_registry.py
    deleted    sphinx-docs/programming-areas/registry/walk-the-registry.rst
    deleted    sphinx-docs/programming-areas/registry/walk_the_registry.py
    deleted    sphinx-docs/programming-areas/registry/walk_the_registry_writer.py
    deleted    sphinx-docs/programming-areas/registry/write-and-read-the-registry.rst
    deleted    sphinx-docs/programming-areas/registry/write_the_registry.py
    deleted    sphinx-docs/resources.rst
    deleted    sphinx-docs/third-party-modules

--- pywin32-docs/sphinx-docs/programming-areas/registry/delete_the_registry_shell.py
+++
@@ -1,9 +0,0 @@
-import _winreg
-import ctypes
-from ctypes import wintypes
-
-shlwapi = ctypes.windll.shlwapi
-SHDeleteKey = shlwapi.SHDeleteKeyW
-HKCU = wintypes.HKEY (_winreg.HKEY_CURRENT_USER)
-
-SHDeleteKey (HKCU, ur"Software\PySoft")
